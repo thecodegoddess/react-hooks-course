a875fef77b13a354acd24b661c4615cdd58c0de2
amber <amber.x.madigan@disney.com>
2019-06-21 15:50:32 -0700

    Chapter 6 - intro to ise effect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const App = () => {
   const [
@@ -6,23 +6,21 @@ const App = () => {
     setDishName,
   ] = useState('');
 
+  useEffect(() => {
+    document.title = `Dish name is ${dishName}`;
+  });
+
   return (
     <div className="main-wrapper">
       <h1>Level Up Dishes</h1>
       <form onSubmit={(e) => {
         e.preventDefault();
-        formSubmit(dishName, setDishName);
       }}>
         <input type="text" onChange={(event) => setDishName(event.target.value)} value={dishName} />
         <button>Submit The Form</button>
       </form>
     </div>
   );
-};
-
-const formSubmit = (value, setValue) => {
-  console.log(`email has been sent to ${value}`);
-  setValue('');
 };
 
 export default App;
