feb6cda8decdddcee8561aa99ebd0888e9a830bd
amber <amber.x.madigan@disney.com>
2019-06-21 12:38:22 -0700

    chapter 3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
+  const [
+    dishName,
+    setDishName,
+  ] = useState('');
+
   return (
     <div className="main-wrapper">
       <h1>Level Up Dishes</h1>
+      <input type="text" onChange={(event) => setDishName(event.target.value)} value={dishName} />
     </div>
   );
 };
