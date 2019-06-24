cb9721b293a7f6a825b4b2bae085e204773b2b3c
amber <amber.x.madigan@disney.com>
2019-06-24 15:41:41 -0700

    Chapter 13 - fetching data - issues
    We are fetching data with useEffect but it is calling it on every single render

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import { Counter } from './Counter';
-import React, { useRef } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import Toggle from './Toggle';
 import { useTitleInput } from './hooks/useTitleInput';
 
 const App = () => {
-  const [dishName, setDishName] = useTitleInput('');
-
+  const [name, setName] = useTitleInput('');
+  const [dishes, setDishes] = useState([]);
   const ref = useRef();
+
+  const fetchDishes = async () => {
+    console.log('fetching data');
+    const res = await fetch('https://my-json-server.typicode.com/leveluptuts/fakeapi/dishes');
+    const dishes = await res.json();
+    setDishes(dishes);
+  };
+
+  useEffect(() => {
+    // fetchDishes(); // Calls on every render over and over again
+  });
+
 
   return (<div className="main-wrapper" ref={ref}>
       <h1 onClick={() => ref.current.classList.toggle('fake-class-here')}>
@@ -17,9 +29,20 @@ const App = () => {
       <form onSubmit={(e) => {
         e.preventDefault();
       }}>
-        <input type="text" onChange={(event) => setDishName(event.target.value)} value={dishName}/>
+        <input type="text" onChange={(event) => setName(event.target.value)} value={name}/>
         <button>Submit The Form</button>
       </form>
+    { dishes && dishes.map((dish) => {
+      return(
+        <article key={dish.name} className="dish-card dish-card--withImage">
+          <h3>{dish.name}</h3>
+          <p>{dish.desc}</p>
+          <div className="ingredients">
+            {dish.ingredients && dish.ingredients.map(ingredient => (<span key={`${dish.name}-${ingredient}`}>{ingredient}</span>))}
+          </div>
+        </article>
+      )
+    })}
     </div>);
 };
 
