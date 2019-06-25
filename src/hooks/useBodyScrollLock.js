7fea559d46efaa12d0cc44372c5366384c4b0db0
amber <amber.x.madigan@disney.com>
2019-06-25 16:42:23 -0700

    Chapter 17 - clean up

--- src/hooks/useBodyScrollLock.js
+++ src/hooks/useBodyScrollLock.js
@@ -2,8 +2,13 @@ import { useLayoutEffect } from 'react';
 
 function useBodyScrollLock() {
   useLayoutEffect(() => {
+    const originalOverflowValue = window.getComputedStyle(document.body).overflow;
     document.body.style.overflow = 'hidden';
-  });
+
+    return () => {
+      document.body.style.overflow = originalOverflowValue;
+    }
+  }, []);
 }
 
 export {
